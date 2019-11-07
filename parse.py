79371fcf3217ab75055bf98cdbb3656aecabedf0
heimdall-syno <63456651+heimdall-syno@users.noreply.github.com>
2019-11-07 22:33:08 +0100

    Copy video file to handbrake watch directory

--- parse.py
+++ parse.py
@@ -6,64 +6,65 @@ from collections import namedtuple
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-cfg = namedtuple('cfg', 'port ip url server_logs client_logs mapping')
+cfg = namedtuple('cfg', 'port ip url server_logs client_logs handbrake mapping')
 cfg.__new__.__defaults__ = (None,) * len(cfg._fields)
 
 synoindex_modes = ['a']
 
 def get_docker_ip(mode):
-    ''' Get the docker0 IP address with or without external packages. '''
+	''' Get the docker0 IP address with or without external packages. '''
 
-    if mode == "server":
-        import netifaces as ni
-        ni.ifaddresses('docker0')
-        ip = ni.ifaddresses('docker0')[ni.AF_INET][0]['addr']
-    else:
-        import subprocess
-        ps = subprocess.Popen(('ip', 'route', 'show'), stdout=subprocess.PIPE)
-        output = subprocess.check_output(('grep', 'default'), stdin=ps.stdout)
-        ps.wait()
-        ip = re.search('([0-9]{1,3}[\.]){3}[0-9]{1,3}', output, flags=0).group()
-    return ip
+	if mode == "server":
+		import netifaces as ni
+		ni.ifaddresses('docker0')
+		ip = ni.ifaddresses('docker0')[ni.AF_INET][0]['addr']
+	else:
+		import subprocess
+		ps = subprocess.Popen(('ip', 'route', 'show'), stdout=subprocess.PIPE)
+		output = subprocess.check_output(('grep', 'default'), stdin=ps.stdout)
+		ps.wait()
+		ip = re.search(r'([0-9]{1,3}[\.]){3}[0-9]{1,3}', output, flags=0).group()
+	return ip
 
 def parse_cfg(config_file, mode):
-  ''' Parse all configuration options of the config file. '''
+	''' Parse all configuration options of the config file. '''
 
-  ## Read the config file
-  config = ConfigParser()
-  config.read(config_file)
-  secs = ['Server', 'Client', 'Mapping']
-  _ = [exit('Error: Section missing') for s in secs if not config.has_section(s)]
+	## Read the config file
+	config = ConfigParser()
+	config.read(config_file)
+	secs = ['Server', 'Client', 'Mapping']
+	_ = [exit('Error: Section missing') for s in secs if not config.has_section(s)]
 
-  ## Get all server configurations from config file
-  ip = get_docker_ip(mode)
-  port = int(config.get(secs[0], 'server_port'))   
-  url = "http://%s:%s" % (ip, port)
-    
-  ## Get the debug files
-  server_logs = config.get(secs[0], 'server_logs')
-  client_logs = config.get(secs[1], 'client_logs')
+	## Get all server configurations from config file
+	ip = get_docker_ip(mode)
+	port = int(config.get(secs[0], 'server_port'))
+	url = "http://%s:%s" % (ip, port)
 
-  ## Get all mappings from the config
-  mapping = config._sections['Mapping']
-  mapping = [m for m in list(mapping.items()) if m[0] != "__name__"]
+	## Get the debug files
+	server_logs = config.get(secs[0], 'server_logs')
+	client_logs = config.get(secs[1], 'client_logs')
+	handbrake = config.get(secs[1], 'handbrake')
 
-  ## Check whether all mappings really exist
-  for m in mapping:
-    check_path = m[1] if mode == "server" else m[0]
-    if not os.path.exists(check_path):
-      logger.error("Error: Mapping path doesnt exist: \"" + check_path + "\"")
-      exit()
-  
-  return cfg(port, ip, url, server_logs, client_logs, mapping)
+	## Get all mappings from the config
+	mapping = config._sections['Mapping']
+	mapping = [m for m in list(mapping.items()) if m[0] != "__name__"]
+
+	## Check whether all mappings really exist
+	for m in mapping:
+		check_path = m[1] if mode == "server" else m[0]
+		if not os.path.exists(check_path):
+			logger.error("Error: Mapping path doesnt exist: \"" + check_path + "\"")
+			exit()
+
+	return cfg(port, ip, url, server_logs, client_logs, handbrake, mapping)
 
 def validate_input(mapping, option, filepath):
-    """ Validate the query arguments. """
+	""" Validate the query arguments. """
 
-    ## General validation
-    if(option not in synoindex_modes): return -1 
-    if not any(m[0] in filepath for m in mapping): return -2
+	## General validation
+	if(option not in synoindex_modes): return -1
+	if not any(m[0] in filepath for m in mapping): return -2
 
-    ## Replace the docker path to the host path
-    for m in mapping: filepath = filepath.replace(m[0], m[1])
-    return filepath
+	## Replace the docker path to the host path
+	for m in mapping: filepath = filepath.replace(m[0], m[1])
+	return filepath
--- post_processing.py
+++ post_processing.py
@@ -1,16 +1,18 @@
-import os, fnmatch, argparse, subprocess, logging
-from subprocess import Popen, PIPE, STDOUT
-from shutil import copy
+import os, fnmatch, argparse, subprocess, logging, sys, shlex, re
+from subprocess import Popen, PIPE, STDOUT, call
+from shutil import copy, copyfile
+
+from mediainfo import ffprobe_file
 from parse import parse_cfg
 
 ## Parse the config
-config_file = os.path.dirname(os.path.abspath(__file__)) + '/config.txt' 
+config_file = os.path.dirname(os.path.abspath(__file__)) + '/config.txt'
 cfg = parse_cfg(config_file, "client")
 
 ## Setup the client logging file
 client_log = "%s/%s" % (cfg.client_logs, "client.log")
-logging.basicConfig(filename=client_log, filemode='a', 
-                    format='%(asctime)s - %(levelname)s: %(message)s')
+logging.basicConfig(filename=client_log, filemode='a',
+					format='%(asctime)s - %(levelname)s: %(message)s')
 
 ## Set the logger and its level
 logger = logging.getLogger(__name__)
@@ -19,11 +21,11 @@ logger.setLevel(logging.DEBUG)
 allowed_ext = ["mkv", "mp4"]
 synoclient_path = "/data/synoindex_scripts/client.py"
 
-def files_with_ext(path, ext):
+def find_files_with_extension(path, ext):
 	''' Find all files in the given path with the extension. '''
 
 	ext_files = []
-	for root, dirnames, filenames in os.walk(path):
+	for root, _, filenames in os.walk(path):
 		for filename in fnmatch.filter(filenames, "*.%s" % ext):
 			if ("sample" not in filename):
 				ext_files.append(os.path.join(root, filename))
@@ -32,7 +34,7 @@ def files_with_ext(path, ext):
 def add_file_to_syno(file_path, synoclient_path):
 	''' Add a file to the SynoIndex via webserver. '''
 
-	process = subprocess.Popen(["python", synoclient_path, "-o", "a", "-f", file_path], 
+	process = subprocess.Popen(["python", synoclient_path, "-o", "a", "-f", file_path],
 							   stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	stdout, stderr = process.communicate()
 	if(stderr): print(stderr)
@@ -40,7 +42,7 @@ def add_file_to_syno(file_path, synoclient_path):
 
 def create_dir_owner(args):
 	''' Create a directory by the file name and change owner. '''
-	
+
 	dir_name = ".".join(args.name.split(".")[:-1])
 	new_dir_path = os.path.join(args.directory, dir_name)
 	if not os.path.exists(new_dir_path):
@@ -48,73 +50,99 @@ def create_dir_owner(args):
 		os.chown(new_dir_path, args.userid, args.groupid)
 	return new_dir_path
 
-def copy_file(new_dir, args):
+def copy_file(src, dst, name, args):
 	''' Copy file to directory and change owner. '''
-	
-	abs_path = os.path.join(args.directory, args.name)
-	new_file_path = os.path.join(new_dir, args.name)
-	if not os.path.exists(new_file_path):
-		copy(abs_path, new_dir)
+
+	## Without renaming the file
+	file_name = os.path.basename(src)
+	new_file_path = os.path.join(dst, file_name)
+	if not name and not os.path.exists(new_file_path):
+		copy(src, dst)
 		os.chown(new_file_path, args.userid, args.groupid)
-	return new_dir
+		return dst
+
+	## Copy and rename the file
+	dst_name = os.path.join(dst, name)
+	if name and not os.path.exists(dst_name):
+		copyfile(src, dst_name)
+		os.chown(dst_name, args.userid, args.groupid)
+		return dst
+
+	return 0
+
+def copy_file_args(dst, args):
+	''' Copy file to directory and change owner. '''
+
+	## Copy the video file to the specified destination
+	new_file = copy_file(args.directory, dst, 0, args)
+	if not new_file:
+		logging.error("Could not copy file (%s) to destination (%s)" % (args.directory, dst))
+		return 0
+
+	return new_file
 
 def copy_file_to_handbrake(src, args):
-    ''' Copy file to the handbrake watch directory and change owner. '''
+	''' Copy file to the handbrake watch directory and change owner. '''
 
-    src_name = os.path.basename(src)
-    dst = os.join("handbrake", "watch", src_name)
-    if not os.path.exists(dst):
-        copy(src, dst)
-        os.chown(dst, args.userid, args.groupid)
-    return dst
-        
+	## Get all media info about the file
+	info = ffprobe_file(src)
+	codec = info["video_codec"]
+
+	## Check whether it is x264 based
+	if codec != "h264" and codec != "x264":
+		return 0
+
+	## Copy the video file to the handbrake watch directory
+	src_dir = src.split(os.sep)[-2]
+	dst_name = "%s%s" % (src_dir, os.path.splitext(src)[1])
+	new_file = copy_file(src, cfg.handbrake, dst_name, args)
+	if not new_file:
+		logging.error("Could not copy file (%s) to handbrake watch directory" % (src))
+		return 0
+	logger.debug("Copied file (%s) to handbrake watch directory" % (src))
+
+	return new_file
+
 def post_processing(args):
-    ''' Post processing '''
+	''' Post processing '''
 
-    ## Get the absolute path
-    abs_path = os.path.join(args.directory, args.name)
+	## If torrent is a single file create a directory and copy that file
+	abs_path = os.path.join(args.directory, args.name)
+	if os.path.isfile(abs_path):
+		new_dir = create_dir_owner(args)
+		abs_path = copy_file_args(new_dir, args)
 
-    ## If torrent is a single file
-    if os.path.isfile(abs_path):
+	## If there are RAR files extract them into the top directory
+	rar_files = find_files_with_extension(abs_path, "rar")
+	logger.debug("Found some rar files: " + ", ".join(rar_files))
+	for rar_file in rar_files:
+		logger.debug("rar file \"%s\", try to unrar it" % (rar_file))
+		process = Popen(["unrar", "x", "-o+", rar_file, abs_path], stdout=PIPE, stderr=PIPE)
+		stdout, stderr = process.communicate()
+		logger.debug(stderr)
 
-        ## Create new directory
-        new_dir = create_dir_owner(args)
+	## Import all non-compressed video files
+	video_files = [find_files_with_extension(abs_path, ext) for ext in allowed_ext]
+	video_files = [i for sl in video_files for i in sl]
+	for video in video_files:
+		add_file_to_syno(video, synoclient_path)
 
-        ## Copy the file to the new directory
-        abs_path = copy_file(new_dir, args)
+	## If the video file is x264-based copy it to the watch directory of the handbrake
+	## docker container
+	for video in video_files:
+		copy_file_to_handbrake(video, args)
 
-    ## If there are RAR files extract them into the top directory
-    rar_files = files_with_ext(abs_path, "rar")
-    logger.debug("Found some rar files: " + ", ".join(rar_files))
-    for rar_file in rar_files:
-        logger.debug("rar file \"%s\", try to unrar it" % (rar_file))
-        process = Popen(["unrar", "x", "-o+", rar_file, abs_path], stdout=PIPE, stderr=PIPE)
-        stdout, stderr = process.communicate()
-        logger.debug(stderr)
-
-    ## Import all non-compressed video files
-    video_files = [files_with_ext(abs_path, ext) for ext in allowed_ext]
-    video_files = [i for sl in video_files for i in sl]
-    for video in video_files:
-        add_file_to_syno(video, synoclient_path)
-        
-    ## After the original video files were added to the VS, check whether its x264 codec based
-    ## If so then copy it to the watch directory of the handbrake docker container
-    for video in video_files:
-        if ("x264" in src):
-            copy_file_to_handbrake(video, args)
-		
 def main():
 	args = argparse.Namespace()
 	parser = argparse.ArgumentParser(description='Post Processing of torrents via transmission')
 	parser.add_argument('-n','--name', help='Name of the torrent', required=True)
 	parser.add_argument('-d','--directory', help='Directory of the torrent', required=True)
 	parser.add_argument('-u','--userid', help='ID of the user (PUID)', type=int, required=True)
-	parser.add_argument('-g','--groupid', help='ID of the group (PGID)', type=int, required=True)	
+	parser.add_argument('-g','--groupid', help='ID of the group (PGID)', type=int, required=True)
 	args = parser.parse_args()
 
 	## Post Processing
 	post_processing(args)
-	
+
 if __name__ == "__main__":
 	main()
