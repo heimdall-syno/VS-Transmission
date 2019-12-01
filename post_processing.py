fb4d36a00cb696f616e2faddd907b40131843bcc
heimdall-syno <63456651+heimdall-syno@users.noreply.github.com>
2019-12-01 15:56:59 +0100

    Add additional info to convert file
    - The handbrake container has a limited perspective on the filesystem due to the shared directories are not mounted. Thus it gets all needed information through the convert file

--- post_processing.py
+++ post_processing.py
@@ -1,128 +1,90 @@
 import os, sys, fnmatch, argparse, subprocess
-from subprocess import Popen, PIPE, STDOUT, call
-from shutil import copy, copyfile
 
 ## Add modules from the submodule (vs-utils)
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(cur_dir, "VS-Utils"))
 sys.path.append(os.path.join(cur_dir, "VS-SynoIndex"))
+from files import files_find_ext, file_copy, file_copy_args
+from files import directory_create_owner, unrar_files
+from parse import parse_cfg, parse_dockerpath
 from mediainfo import ffprobe_file
-from parse import parse_cfg
 from client import client
 
 ## Parse the config
 config_file = os.path.join(cur_dir, "config.txt")
 cfg = parse_cfg(config_file, "vs-transmission", "docker")
 
-#####################################################################
-###                    File system functions                      ###
-#####################################################################
+def write_convert_file(source, source_host, root_host, output_host):
 
-def files_find_ext(path, ext):
-	''' Find all files in the given path with the extension. '''
+	## Create convert file path
+	convert_file = "%s.txt" % (".".join(os.path.basename(source).split(".")[:-1]))
+	convert_file = os.path.join(cfg.handbrake, "convert", convert_file)
 
-	ext_files = []
-	for root, _, filenames in os.walk(path):
-		for filename in fnmatch.filter(filenames, "*.%s" % ext):
-			if ("sample" not in filename):
-				ext_files.append(os.path.join(root, filename))
-	return ext_files
+	## Write the convert file
+	convert_content = "root_host:%s\nsource_host:%s\noutput_host:%s" % (root_host, source_host, output_host)
+	with open(convert_file, 'w+') as f: f.write(convert_content)
+	print("Created convert file (%s)" % (convert_file))
 
-def file_copy(src, dst, args):
-	''' Copy file to directory and change owner. '''
-
-	## Without renaming the file
-	file_name = os.path.basename(src)
-	new_file_path = os.path.join(dst, file_name)
-	if not os.path.exists(new_file_path):
-		copy(src, dst)
-		os.chown(new_file_path, args.userid, args.groupid)
-		return dst
-
-	return 0
-
-def file_copy_args(dst, args):
-	''' Copy file to directory and change owner. '''
-
-	## Copy the video file to the specified destination
-	new_file = file_copy(args.directory, dst, args)
-	if not new_file:
-		print("Error: Could not copy file (%s) to destination (%s)" % (args.directory, dst))
-		return 0
-
-	return new_file
-
-def directory_create_owner(args):
-	''' Create a directory by the file name and change owner. '''
-
-	dir_name = ".".join(args.name.split(".")[:-1])
-	new_dir_path = os.path.join(args.directory, dir_name)
-	if not os.path.exists(new_dir_path):
-		os.mkdir(new_dir_path)
-		os.chown(new_dir_path, args.userid, args.groupid)
-	return new_dir_path
-
-#####################################################################
-###                      Handbrake functions                      ###
-#####################################################################
-
-def copy_file_to_handbrake(src, args):
+def copy_file_to_handbrake(args, source, source_host, root_host):
 	''' Copy file to the handbrake watch directory and change owner. '''
 
 	## Get all media info about the file
-	codec = ffprobe_file(src)["video_codec"]
+	codec = ffprobe_file(source)["video_codec"]
 
-	## Check whether it is x264 based
-	if codec != "h264" and codec != "x264":
-		return 0
+	## Check whether it is one codec of the config is present
+	if codec not in cfg.codecs:
+		return
 
 	## Copy the video file to the handbrake watch directory
 	watch_dir = os.path.join(cfg.handbrake, "watch")
-	new_file = file_copy(src, watch_dir, args)
-	if not new_file:
-		print("Error: Could not copy file (%s) to handbrake watch directory" % (src))
-		return 0
-	print("Copied file (%s) to handbrake watch directory" % (src))
+	output = file_copy(source, watch_dir, args)
+	output_host = parse_dockerpath(cfg.mapping, output)
+	if not output:
+		print("Error: Could not copy file (%s) to handbrake watch directory" % (source))
+		return
+	print("Copied file (%s) to handbrake watch directory" % (source))
 
-	## Create convert-file to note the original path
-	file_name = "%s.txt" % (".".join(os.path.basename(src).split(".")[:-1]))
-	convert_file = os.path.join(cfg.handbrake, "convert", file_name)
-	with open(convert_file, 'w+') as f: f.write('%s' % src)
-	print("Create convert file (%s)" % (convert_file))
+	## Write the convert file with all necessary information
+	write_convert_file(source, source_host, root_host, output_host)
 
-	return new_file
+def fix_single_file(args):
+	""" If a single video file was downloaded create a directory and copy the file
+
+	Arguments:
+		args {Namespace} -- Namespace containing all shell arguments
+
+	Returns:
+		string -- Path of the copied file.
+	"""
+
+	abs_path = os.path.join(args.directory, args.name)
+	if os.path.isfile(abs_path):
+		new_dir = directory_create_owner(args)
+		abs_path = file_copy_args(new_dir, args)
+	return abs_path
 
 def post_processing(args):
 	''' Post processing '''
 
 	## If torrent is a single file create a directory and copy that file
-	abs_path = os.path.join(args.directory, args.name)
-	if os.path.isfile(abs_path):
-		new_dir = directory_create_owner(args)
-		abs_path = file_copy_args(new_dir, args)
+	abs_path = fix_single_file(args)
 
 	## If there are RAR files extract them into the top directory
-	rar_files = files_find_ext(abs_path, "rar")
-	print("Found some rar files: " + ", ".join(rar_files))
-	for rar_file in rar_files:
-		print("rar file \"%s\", try to unrar it" % (rar_file))
-		process = Popen(["unrar", "x", "-o+", rar_file, abs_path], stdout=PIPE, stderr=PIPE)
-		stdout, stderr = process.communicate()
-		print(stderr)
+	unrar_files(abs_path)
 
 	## Import all non-compressed video files
-	video_files = [files_find_ext(abs_path, ext) for ext in ["mkv", "mp4"]]
-	video_files = [i for sl in video_files for i in sl]
-	for video in video_files:
-		video_hostpath = parse_dockerpath(cfg.mapping, video)
-		client(video_hostpath)
+	source_files = [files_find_ext(abs_path, ext) for ext in ["mkv", "mp4"]]
+	source_files = [i for sl in source_files for i in sl]
+	for source in source_files:
+		source_host = parse_dockerpath(cfg.mapping, source)
+		client(source_host)
 
 	## If the video file is x264-based copy it to the watch directory of the handbrake
 	## docker container
 	if (args.handbrake):
-		for video in video_files:
-			video_hostpath = parse_dockerpath(cfg.mapping, video)
-			copy_file_to_handbrake(video_hostpath, args)
+		for source in source_files:
+			(source_host, root_host) = parse_dockerpath(cfg.mapping, source)
+			copy_file_to_handbrake(args, source, source_host, root_host)
 
 def main():
 
