c2f71ddcce72f1886ea5097ebe5efa4d4bf68dbf
heimdall-syno <63456651+heimdall-syno@users.noreply.github.com>
2020-01-19 21:34:07 +0100

    One click setup

---
+++ autogen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## Check for sudo rights
+if [ "$EUID" -ne 0 ]
+  then printf "Please rerun as root:\n \$ sudo ./autogen.sh\n"
+  exit
+fi
+
+## Host - Install pip for python3 and all needed modules
+curl https://bootstrap.pypa.io/get-pip.py -o /get-pip.py
+python3 /get-pip.py && rm -rf /get-pip.py
+export PATH=$PATH:/volume1/@appstore/py3k/usr/local/bin
+pip3 install -r requirements.txt
+
+## Container - Install python3 and pip and all needed modules
+containername=`docker ps | grep -i transmission | awk '{print $NF}'`
+docker exec -it "$containername" apt-get install python3 ffmpeg
+docker exec -it "$containername" curl https://bootstrap.pypa.io/get-pip.py -o /get-pip.py
+docker exec -it Transmission python3 /get-pip.py &&  rm -rf /get-pip.py
+docker exec -it Transmission pip3 install configparser
