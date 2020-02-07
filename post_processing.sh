cba03e73abb89eabc978159e09af1cd50752cf08
heimdall-syno <63456651+heimdall-syno@users.noreply.github.com>
2020-02-07 19:35:03 +0100

    Execute post processing with python3

--- post_processing.sh
+++ post_processing.sh
@@ -6,4 +6,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Execute the post processing via Python
-python "$DIR/post_processing.py" -d "$TR_TORRENT_DIR" -n "$TR_TORRENT_NAME" -u "$PUID" -g "$PGID"
+python3 "$DIR/post_processing.py" -d "$TR_TORRENT_DIR" -n "$TR_TORRENT_NAME" -u "$PUID" -g "$PGID"
